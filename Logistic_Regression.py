801386ed5e256cf861f03f8473a88c74f682dfc7
pbavafa <pouya.bavafa@gmail.com>
2018-12-31 14:09:41 -0800

    logistic ML

---
+++ Logistic_Regression.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('f150_XLT_cars.csv')
+
+#sns.jointplot('Year', 'odometer', data=data)
+sns.heatmap(data.corr())
